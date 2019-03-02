158c9ce62a5cc3eb0c36b7c29964f3dd1be3409d
Sena <senaaltinkaynak.be@gmail.com>
2019-03-02 00:01:05 -0500

    Reorganize annotations and close driver

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Locale;
@@ -17,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 public class LoginTests {
             WebDriver driver;
 
-            @BeforeClass
+            @BeforeMethod
             public void setUp(){
                 Locale.setDefault(new Locale("en", "US"));
                 WebDriverManager.chromedriver().setup();
@@ -33,27 +34,6 @@ public class LoginTests {
                 Assert.assertTrue(driver.getTitle().equals("Web Orders"));
             }
 
-    @Test
-    public void LogOutTest(){
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        driver.findElement(By.id("ctl00_logout")).click();
-        String title=driver.getTitle();
-        Assert.assertEquals(driver.getTitle(), "Web Orders Login Status");
-
-    }
-
-    @Test
-    public void negativeloginTest() {
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
-        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
-
-        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
-
-    }
     @AfterMethod
     public void CleanUp(){
         driver.close();
